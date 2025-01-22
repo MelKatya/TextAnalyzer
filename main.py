74bfae8ad2e8c02da420efc2805fad7d664abb40
Katerina <mel.katya@mail.ru>
2025-01-22 16:41:26 +0300

    Checking for stop words (almost), top list, words in alphabetical order after frequency

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import re
 from typing import Dict, List, Tuple
 
 
-def words_count(text: str, result: Dict[str, int]) -> Dict[str, int]:
+def words_count(text: str, result: Dict[str, int], stop_words) -> Dict[str, int]:
     """
     Заполняет словарь частоты слов из строки текста.
 
@@ -17,10 +17,11 @@ def words_count(text: str, result: Dict[str, int]) -> Dict[str, int]:
     template = re.compile(r'[^\d,\W]+[-]?\w*')
     words = template.findall(text.lower())
     for word in words:
-        if result.get(word):
-            result[word] += 1
-        else:
-            result[word] = 1
+        if word not in stop_words:
+            if result.get(word):
+                result[word] += 1
+            else:
+                result[word] = 1
     return result
 
 
@@ -49,6 +50,8 @@ def main_menu() -> Dict[str, int]:
         Dict[str, int]: словарь из слов и их частоты из выбранного источника.
     """
 
+    stop_words = fill_stop_words()
+
     while True:
         print('Укажите источник текста:\n'
               '1 - Текстовый файл;\n'
@@ -56,14 +59,47 @@ def main_menu() -> Dict[str, int]:
         text_source = input('Ввод: ')
 
         if text_source == '1':
-            return load_file()
+            return load_file(stop_words)
         elif text_source == '2':
             return write_text()
         else:
             print('Неверно указан источник текста. Введите 1 или 2.')
 
 
-def load_file() -> Dict[str, int]:
+def fill_stop_words() -> List[str]:
+    """
+    Заполняет список со стоп-словами, если это необходимо.
+
+    Returns:
+        List[str]: список стоп-слов.
+    """
+
+    while True:
+        print('Стоп-слова - слова, которые не будут учитываться при подсчете частоты слов.\n'
+              '1 - если необходима фильтрация стоп-слов.\n'
+              '0 - если фильтрация не нужна.')
+        user_choice = input('Ввод: ')
+
+        if user_choice == '0':
+            return []
+
+        elif user_choice == '1':
+            while True:
+                stop_words = input('Введите стоп-слова через пробел: ')
+
+                control_check = re.findall(r'[^\d,\W]+[-]?\w*', stop_words)
+                stop_words = stop_words.split()
+
+                if control_check == stop_words:
+                    return stop_words
+                else:
+                    print('Ошибка ввода. В словах могут присутствовать только буквы и дефис("-").')
+
+        else:
+            print('Неверно сделан выбор. Введите 1 или 0.')
+
+
+def load_file(stop_words) -> Dict[str, int]:
     """
     Загружает текст из файла и подсчитывает частоту слов.
 
@@ -78,7 +114,7 @@ def load_file() -> Dict[str, int]:
     try:
         with open(my_file, 'r', encoding='utf-8') as file:
             for line in file:
-                words = words_count(line, words)
+                words = words_count(line, words, stop_words)
 
         return words
 
@@ -133,21 +169,50 @@ def selection_sorting() -> bool:
     return flag
 
 
+def top_words(words, reverse):
+    top = []
+    if reverse:
+        top = words[:10]
+    else:
+        top = words[-1:-11:-1]
+    return top
+
+
+def frequency_thresholds(words):
+    """!!!!!! доделать"""
+    print('Исключение редко встречающихся слов (например, с частотой < N).')
+    print('Для исключения слов из списка с частотой N и меньше, введите N')
+    print('Если исключать слова из списка не нужно, введите 0')
+    number_n = int(input('Введите N: '))
+    if number_n > 0:
+        new_words = [pair for pair in words if pair[1] > number_n]
+    else:
+        new_words = words[:]
+    return new_words
+
+
+
 words_dict = main_menu()
 
 reverse_flag = selection_sorting()
 
-sorted_words = sorted(words_dict.items(), key=lambda w: (w[1], w[0]), reverse=reverse_flag)
+# Если reverse_flag == True, то (-1)^1, т.е. сортировка производится по убыванию, иначе - по возрастанию
+sorted_words = sorted(words_dict.items(), key=lambda w: ((-1) ** reverse_flag * w[1], w[0]))
+print(sorted_words)
 
 if len(words_dict) <= 500:
     print_result(sorted_words)
 else:
-    print('Количество слов превышает 500. Результат будет записан в файл "result.txt".')
+    print('Количество слов превышает 500. Результат можете посмотреть в файле "result.txt".')
 
 with open('result.txt', 'w', encoding='utf-8') as file:
     print_result(sorted_words, file)
 
 
 print('Общее количество слов:', sum(words_dict.values()))
-# print('Топ-10 слов:')
-# print(dict(sorted_words[:10]))
+
+print('Топ-10 слов:')
+print_result(top_words(sorted_words, reverse_flag))
+
+new_words = frequency_thresholds(sorted_words)
+print_result(new_words)
